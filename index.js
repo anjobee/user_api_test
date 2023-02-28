41ffbc61ea7224568ea101ac943e7da6c754d380
Anjo Bihis <angelo.bihis@maroonstudios.com>
2023-03-01 00:46:58 +0800

    fix: remove duplicate connection.end

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const { connection } = require('./db')('user_api_db')
 
-process.on('SIGINT', () => {
-  console.log('Closing MySQL connection...');
-
-  db.end();
-});
-
 const app = require('./app')(connection);
 
 app.listen(8080, () => console.log(`App listening to port 8080`));
